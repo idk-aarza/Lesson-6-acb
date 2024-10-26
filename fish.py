f9570227ed50f676030f7f60c6ad33f4268412f6
idk-aarza <aarzak2012@gmail.com>
2024-10-26 12:03:06 +0530

    fishy

---
+++ fish.py
@@ -0,0 +1,6 @@
+user_input=input("Enter something")
+#Check if the input is a digit
+if user_input.isdigit():
+    print('The input',user_input,'is a digit.')
+else:
+  print('The input',user_input, 'is not a digit.')
